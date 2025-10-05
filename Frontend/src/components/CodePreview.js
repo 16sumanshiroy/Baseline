e59d734709f963cd7f264c49b97006dbe3b01d3a
16sumanshiroy <112249799+16sumanshiroy@users.noreply.github.com>
2025-10-06 02:35:57 +0530

    CodePreview.js

--- Frontend/src/components/CodePreview.js
+++ Frontend/src/components/CodePreview.js
@@ -22,7 +22,7 @@ export default function CodePreview({ yaml, loading, meta }) {
     setExplanation("");
 
     try {
-      const res = await fetch("http://localhost:5000/explain", {
+      const res = await fetch("https://baseline-backend-931f.onrender.com/explain", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ yaml }),
