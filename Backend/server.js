60d1c4a3fea24ae75b93f59c0e47834d4926c179
16sumanshiroy <112249799+16sumanshiroy@users.noreply.github.com>
2025-10-06 03:03:18 +0530

    server.js

--- Backend/server.js
+++ Backend/server.js
@@ -137,7 +137,7 @@ Caching is configured for faster builds and improved performance.
 });
 
 // ======================= SERVER START ==========================
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () =>
   console.log(`✅ Server running on port ${PORT}`)
 );
